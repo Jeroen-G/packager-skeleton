1d056754624d0137248e6a7fe7a032ea032fcdba
JeroenG <jeroengjeroeng@gmail.com>
2017-10-17 15:12:48 +0200

    wip

---
+++ MyPackageServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace :uc:vendor\:uc:package;
+
+use Illuminate\Support\ServiceProvider;
+
+class :uc:packageServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
