ebb0b90070dfe8b62f22725d3fe67671c1b606ec
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-12-11 15:29:58 -0300

    Update ExampleTest.php
    Test methods must starts with "test" string

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function true_is_true()
+    public function testTrueIsTrue()
     {
         $this->assertTrue(true);
     }
