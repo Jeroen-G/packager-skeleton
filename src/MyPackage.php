ae04e5452f33cb0ded671a99f70f7b885977bf00
Charlie Page <charliepage88@gmail.com>
2020-06-23 19:45:07 -0400

    update

--- src/MyPackage.php
+++ src/MyPackage.php
@@ -2,7 +2,34 @@
 
 namespace :uc:vendor\:uc:package;
 
+use Adaptcms\Auth\Models\Permission;
+use Adaptcms\Base\Models\Package;
+
 class :uc:package
 {
-    // Build wonderful things
+  /**
+  * Sync Permissions
+  *
+  * @return void
+  */
+  /*
+  public function syncPermissions()
+  {
+    $permissions = [
+      // list permission names here, such as `admin.:lc:package.index`
+    ];
+
+    Permission::syncPackagePermissions($permissions);
+  }
+  */
+
+  /**
+  * On Install
+  *
+  * @return void
+  */
+  public function onInstall()
+  {
+    Package::syncPackageFolder(get_class());
+  }
 }
